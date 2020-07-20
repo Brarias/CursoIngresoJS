4764689fbfd97207ed537367d9af68659c7c1f78
brarias <braianrodrigo14@gmail.com>
2020-07-20 12:14:17 -0300

    ejercicios de la cuarta clase

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -3,12 +3,16 @@ function mostrar()
 	let edad;
 	
 	edad=parseInt(document.getElementById("txtIdEdad").value);
-	
-	if(edad >= 13 && edad <= 17){
+	// pregunta si estoy dentro del rango
+	/*if(edad >= 13 && edad <= 17){
 	
 		alert("Es adolescente"); 
 	
-	} 
+	} */
+	// pregunta si estoy fuera del rango y lo niego
+	if(!(edad < 13 || edad > 17)){
+		alert("Es adolescente");
+	}
 	
 	alert("Fin de la funcion");
 
