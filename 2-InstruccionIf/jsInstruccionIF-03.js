faa1c2594d159dfeed1ff7653374430fc58959ac
brarias <braianrodrigo14@gmail.com>
2020-07-20 10:28:20 -0300

    ejercicios de la cuarta clase

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,16 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
-
+	let edad;
+	
+	edad=parseInt(document.getElementById("txtIdEdad").value);
+	
+	if(edad >= 18){
+	
+		alert("Es Mayor de edad"); 
+	
+	} else{
+		alert("Es Menor de edad");
+	}
+	alert("Fin de la funcion");
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,15 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-
+	let edad;
+	
+	edad=parseInt(document.getElementById("txtIdEdad").value);
+	
+	if(edad >= 13 && edad <= 17){
+	
+		alert("Es adolescente"); 
+	
+	} 
+	
+	alert("Fin de la funcion");
 
 }//FIN DE LA FUNCIÓN
