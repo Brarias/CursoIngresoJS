84973548f24114c70f252702224b3c1074e4feed
brarias <braianrodrigo14@gmail.com>
2020-07-21 12:05:31 -0300

    ejercicios de la quinta clase

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+let nota;
 
+nota = Math.round(Math.random() * 9 + 1);
+
+if(nota >= 9 ){
+	alert("EXCELENTE " + nota);
+
+}else if(nota >= 4){
+	alert("APROBÓ " + nota);
+}else{
+	alert("Vamos, la proxima se puede " + nota);
+
+}
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,8 +1,38 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes;
+	mes = document.getElementById("txtIdMes").value;
+	
+	switch(mes){
+		case "Enero":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Agosto":
+		case "Octubre":
+		case "Diciembre":
+		   alert("Tiene 31 dias.");
+			break;
+		case "Febrero":
+				alert("Tiene 28 dias");
+
+	 default:
+		alert("Tiene 30 dias.");
+			break;
+		
+		
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
 	
 	
 
